460927aa74dad723c93df07a6c34218e170ef66f
Greg Belanger <agilekalaf@gmail.com>
2020-11-21 20:54:11 +0000

    reformatting to make it all pretty like

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def TakingSouls(miles,souls):
-  if miles > 6:
-    print("Miles run " + str(miles) + ", souls taken " + str(souls) + ".")
-  else:
-    print("Stay Hard, keep running")
-    TakingSouls(miles+1,souls+1)
+def TakingSouls(miles, souls):
+    if miles > 6:
+        print("Miles run " + str(miles) + ", souls taken " + str(souls) + ".")
+    else:
+        print("Stay Hard, keep running")
+        TakingSouls(miles + 1, souls + 1)
 
-TakingSouls(3,0)
+
+TakingSouls(3, 0)
